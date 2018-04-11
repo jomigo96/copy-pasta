15d238131383f27c7b74ff2adf420618a151c547
jomigo96 <jomigo96@yahoo.com>
2018-04-11 18:11:19 +0100

    Lab 5 working

---
+++ src/5-lab/app_teste.c
@@ -0,0 +1,60 @@
+#include "clipboard.h"
+#include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
+int main(){
+
+		int fd = clipboard_connect("./");
+		int opt=1;
+		int entry;
+		int code;
+		
+		if(fd== -1){
+			exit(-1);
+		}
+		char *dados;
+		size_t size = 0;
+
+		
+		while(opt != 0){
+			
+			printf("Copy - 1  Paste - 2   Exit - 0\n");
+			scanf("%d", &opt);
+			while(getchar() != '\n');
+			
+			if (opt == 1){
+				printf("Message: ");
+				getline(&dados, &size, stdin);	
+				printf("Entry: ");
+				scanf("%d", &entry);
+				while(getchar() != '\n');
+				
+				//printf("Calling copy with [%s]\n", dados);
+				
+				code=clipboard_copy(fd, entry, dados, size);
+				if(code != -1)
+					printf("Success\n");
+				else
+					printf("Failure\n");
+			}
+			if (opt == 2){
+				printf("Entry: ");
+				scanf("%d", &entry);
+				while(getchar() != '\n');
+				code=clipboard_paste(fd, entry, dados, size);
+				if(code != -1){
+					printf("Success\n");
+					
+					dados[code]=0;
+					
+					printf("Message: %s\n",dados);
+				}
+				else
+					printf("Failure\n");
+			}
+			
+		}
+		
+		
+		exit(0);
+	}
